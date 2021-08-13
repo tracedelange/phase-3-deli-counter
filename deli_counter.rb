df5345073664c91e2356c36c1d8d68e49d3aaac5
Trace <tracedelange@me.com>
2021-08-12 20:08:25 -0500

    commit tree bb48b11d0dd3dbcc81341365de2a6b0f432a970a

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(array)
+    if array.length == 0
+        puts 'The line is currently empty.'
+    else
+        line = ['The line is currently:']
+        array.each.with_index do |item, index|
+            line.push("#{index + 1}. #{item}")
+        end
+        puts line.join(' ')
+    end
+end
+
+def take_a_number(line, cus)
+    if line.length == 0
+        line.push(cus)
+        return line
+    end
+end
